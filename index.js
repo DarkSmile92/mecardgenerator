9ef6f30f3cdae241d4c36417e6fc198d0c24374d
Robin Kretzschmar <robin.kretzschmar@yahoo.de>
2020-01-20 18:57:42 +0100

    Add babel

--- index.js
+++ index.js
@@ -1,43 +1,13 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-	value: true
+  value: true
 });
-var MeCardGen = function MeCardGen(
-	fName,
-	lName,
-	email,
-	mobile,
-	url,
-	street,
-	city,
-	state,
-	zip,
-	country
-) {
-	return (
-		"MECARD:N:" +
-		fName +
-		"," +
-		lName +
-		";EMAIL:" +
-		email +
-		";TEL:" +
-		mobile +
-		";URL:" +
-		url +
-		";ADR:,," +
-		street +
-		"," +
-		zip +
-		" " +
-		city +
-		"," +
-		state +
-		"," +
-		country +
-		";;"
-	);
+exports["default"] = void 0;
+
+var MeCardGen = function MeCardGen(fName, lName, email, mobile, url, street, city, state, zip, country) {
+  return "MECARD:N:".concat(fName, ",").concat(lName, ";EMAIL:").concat(email, ";TEL:").concat(mobile, ";URL:").concat(url, ";ADR:,,").concat(street, ",").concat(zip, " ").concat(city, ",").concat(state, ",").concat(country, ";;");
 };
 
-exports.default = MeCardGen;
+var _default = MeCardGen;
+exports["default"] = _default;
